eaa1650f897fa6b2854aa62698cd627951dabcc9
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2021-02-16 18:16:49 -0600

    autocomplete

--- src/app/country/pages/country/country.component.ts
+++ src/app/country/pages/country/country.component.ts
@@ -6,12 +6,19 @@ import { CountryService } from './../../services/country.service';
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
-  styles: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
+  ],
 })
 export class CountryComponent implements OnInit {
   public searchTerm = '';
   public isError = false;
   public countries: CountryResponse[] = [];
+  public suggestedCountries: CountryResponse[] = [];
 
   constructor(private countryService: CountryService) {}
 
@@ -32,5 +39,18 @@ export class CountryComponent implements OnInit {
     );
   }
 
-  public suggestions(searchTerm: string): void {}
+  public suggestions(searchTerm: string): void {
+    this.isError = false;
+    this.searchTerm = searchTerm;
+    this.countryService.searchCountry(this.searchTerm).subscribe(
+      (res: CountryResponse[]) => {
+        this.suggestedCountries = res.splice(0,4);
+      },
+      (err: HttpErrorResponse) => {
+        console.error(err.error);
+        this.isError = true;
+        this.suggestedCountries = [];
+      }
+    );
+  }
 }
